1de107f3b7bcf957fb4ee0f44d8b1a76b4d9cef8
Nick Breen <nick@foobar.net.nz>
2015-10-10 18:16:02 +1300

    apache version working!

---
+++ Dockerfile
@@ -0,0 +1,62 @@
+FROM php:5.6-apache
+# See https://github.com/docker-library/wordpress
+
+MAINTAINER Nick Breen <nick@foobar.net.nz>
+
+RUN a2enmod rewrite
+
+# WP-CLI requires less when showing help, ignores $PAGER!
+RUN DEBIAN_FRONTEND=noninteractive apt-get update \
+	&& apt-get install -qy \
+        less \
+        libpng12-dev \
+        libjpeg-dev \
+    && apt-get clean
+
+# Install WP-CLI http://wp-cli.org
+# Install Bash completion for WP-CLI
+RUN PHAR=/usr/local/lib/php/wp-cli.phar &&\
+    curl -sSfLo $PHAR https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&\
+    chmod +x $PHAR &&\
+    ln -s $PHAR /usr/local/bin/wp &&\
+    curl -sSfLo /etc/bash_completion.d/wp-cli https://raw.githubusercontent.com/wp-cli/wp-cli/master/utils/wp-completion.bash
+
+# Install [a] Docker PHP PECL install script
+# Enable the PHP opcache extension
+# Install the PECL OAuth extension
+RUN PECL=/usr/local/bin/docker-php-pecl-install \
+    && curl -sSfLo $PECL https://raw.githubusercontent.com/helderco/docker-php/master/template/bin/docker-php-pecl-install \
+    && chmod +x $PECL \
+    && docker-php-ext-enable opcache \
+    && docker-php-pecl-install oauth \
+    && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
+    && docker-php-ext-install gd \
+    && docker-php-ext-install mysqli
+
+COPY entrypoint.sh oauth.php /
+
+ENV WP_LOCALE "en_NZ"
+ENV WP_DB_HOST "**automatically uses \$MYSQL_PORT_3306_TCP_ADDR**"
+ENV WP_DB_PORT "**automatically uses \$MYSQL_PORT_3306_TCP_PORT**"
+ENV WP_DB_NAME "**automatically uses \$MYSQL_DATABASE**"
+ENV WP_DB_USER "**automatically uses \$MYSQL_USER**"
+ENV WP_DB_PASSWORD "**automatically uses \$MYSQL_PASSWORD**"
+ENV WP_DB_PREFIX "wp_"
+
+ENV WP_URL "http://example.com"
+ENV WP_TITLE "Example Blog"
+ENV WP_ADMIN_USER "admin"
+ENV WP_ADMIN_PASSWORD "**change me**"
+ENV WP_ADMIN_EMAIL "admin@example.com"
+
+ENV WP_THEMES ""
+ENV BB_THEMES ""
+ENV WP_PLUGINS ""
+ENV BB_PLUGINS ""
+
+ENV WP_IMPORT ""
+ENV WP_OPTIONS ""
+
+# Apparently, have to reset the entrypoint and command
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["apache2-foreground"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 WP=$(which wp)
-DEFAULT_BASE_DIR="/var/www/html"
-
-function log {
-  [ "$VERBOSE" ] && echo "$@"
-}
 
 # Dirty wrapper for wp-cli
 function wp {
-  $WP --allow-root "$@"
+	$WP --allow-root "$@"
 }
 
 # Installs themes or plugins from a list on STDIN.
@@ -28,17 +23,17 @@ function wp {
 #   EOT
 #
 function install_a {
-  local A=$1
-  while read SLUG URL;
-  do
-    if [ "$SLUG" -a "$URL" ]
-    then
-      wp $A is-installed $SLUG || wp $A install "$URL"
-    elif [ "$SLUG" ]
-    then
-      wp $A is-installed $SLUG || wp $A install $SLUG
-    fi
-  done
+	local A=$1
+	while read SLUG URL;
+	do
+		if [ "$SLUG" -a "$URL" ]
+		then
+			wp $A is-installed $SLUG || wp $A install "$URL"
+		elif [ "$SLUG" ]
+		then
+			wp $A is-installed $SLUG || wp $A install $SLUG
+		fi
+	done
 }
 
 # Installs themes or plugins specified on STDIN hosted at BitBucket.
@@ -59,126 +54,115 @@ function install_a {
 # 3. Deactivate the old theme/plugin. E.g. wp theme deactivate some_theme_12345678
 # 4. Activate the new theme/plugin. E.g. wp theme activate some_theme_90abcdef
 #
+# TODO fetch [a specific] tagged download from BB
+#
 function install_b {
-  local A=$1
-  while read REPO TAG;
-  do
-    if [ "$REPO" ]
-    then
-      # TODO add support for the 'latest' tag by omission of the tag value
-      local URL="https://bitbucket.org/${REPO}/get/${TAG}.zip"
-      # TODO use a mktemp file for the ZIP and clean up afterwards
-      local ZIP="wp-content/${A}s/${REPO/\//.}.${TAG}.zip"
-      bb $URL > $ZIP || log Tag does not exist for: $REPO @ $TAG && wp $A install $ZIP --force
-    fi
-  done
+	local A=$1
+	while read REPO TAG;
+	do
+		if [ "$REPO" ]
+		then
+			# TODO add support for the 'latest' tag by omission of the tag value
+			local URL="https://bitbucket.org/${REPO}/get/${TAG}.zip"
+			# TODO use a mktemp file for the ZIP and clean up afterwards
+			local ZIP="wp-content/${A}s/${REPO/\//.}.${TAG}.zip"
+			bb $URL > $ZIP || echo Tag does not exist for: $REPO @ $TAG && wp $A install $ZIP --force
+		fi
+	done
 }
 
 # Dirty function to call the oauth.php script
 function bb {
-  php /oauth.php --key "$BB_KEY" --secret "$BB_SECRET" --url "$1"
+	php /oauth.php --key "$BB_KEY" --secret "$BB_SECRET" --url "$1"
 }
 
 function install_core {
-  # Always download the lastest WP
-  wp core download --locale="${WP_LOCALE:-en_NZ}" --force
-  # Configure database 
-  # Fallback to the linked mysql container if no explicit DB host is specified
-  # Assume that a DB has already been created
-  # Skip the DB check as there's unlikely to be a mysql client available
-  wp core config \
-      --skip-check \
-      --dbname="${WORDPRESS_DB_NAME:-wordpress}" \
-      --dbuser="$WORDPRESS_DB_USER" \
-      --dbpass="$WORDPRESS_DB_PASSWORD" \
-      --dbhost="${WORDPRESS_DB_HOST:-$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT}" \
-      --dbprefix="${WORDPRESS_DB_PREFIX:-wp_}" 
+	# Always download the lastest WP
+	wp core download --locale="${WP_LOCALE}" --force
 
-  # Configure the Blog
-  wp core is-installed || wp core install \
-      --url="$WP_URL" \
-      --title="$WP_TITLE" \
-      --admin_user="$WP_ADMIN_USER" \
-      --admin_password="$WP_ADMIN_PASSWORD" \
-      --admin_email="$WP_ADMIN_EMAIL"
+	# Configure database 
+	# Fallback to the explicit DB config if no mysql container is linked
+	# Assume that a DB has already been created
+	# Skip the DB check as there isn't a mysql client available
+	rm -f wp-config.php && wp core config \
+			--skip-check \
+			--dbname="${MYSQL_ENV_MYSQL_DATABASE:-$WP_DB_NAME}" \
+			--dbuser="${MYSQL_ENV_MYSQL_USER:-$WP_DB_USER}" \
+			--dbpass="${MYSQL_ENV_MYSQL_PASSWORD:-$WP_DB_PASSWORD}" \
+			--dbhost="${MYSQL_PORT_3306_TCP_ADDR:-WP_DB_HOST}:${MYSQL_PORT_3306_TCP_PORT:-$WP_DB_PORT}" \
+			--dbprefix="${WP_DB_PREFIX}"
+
+	# Configure the Blog
+	wp core is-installed || wp core install \
+			--url="$WP_URL" \
+			--title="$WP_TITLE" \
+			--admin_user="$WP_ADMIN_USER" \
+			--admin_password="$WP_ADMIN_PASSWORD" \
+			--admin_email="$WP_ADMIN_EMAIL"
 }
 
 function install_themes {
-  install_a theme <<< "$WP_THEMES"
-  install_b theme <<< "$BB_THEMES"
-  wp theme list
+	install_a theme <<< "$WP_THEMES"
+	install_b theme <<< "$BB_THEMES"
+	wp theme list
 }
 
 function install_plugins {
-  install_a plugin <<< "$WP_PLUGINS"
-  install_b plugin <<< "$BB_PLUGINS"
-  wp plugin list
+	install_a plugin <<< "$WP_PLUGINS"
+	install_b plugin <<< "$BB_PLUGINS"
+	wp plugin list
 }
 
 function install {
-  install_core
-  install_themes
-  install_plugins
-  wp plugin activate --all
+	install_core
+	install_themes
+	install_plugins
+	wp plugin activate --all
 }
 
 function upgrade {
-  wp core update \
-      && wp core update-db \
-      && wp theme update --all \
-      && wp plugin update --all
+	wp core update \
+			&& wp core update-db \
+			&& wp theme update --all \
+			&& wp plugin update --all
 
-  # TODO fetch [a specific] tagged download from BB
 }
 
 # Sets options as specified in STDIN.
 # Expects format of OPTION_NAME JSON_STRING
 function options {
-  while read OPTION JSON;
-  do
-    if [ "$OPTION" -a "$JSON" ]
-    then
-      wp option set "$OPTION" "$JSON" --format=json
-    fi
-  done <<< "$WP_OPTIONS"
+	while read OPTION JSON;
+	do
+		if [ "$OPTION" -a "$JSON" ]
+		then
+			wp option set "$OPTION" "$JSON" --format=json
+		fi
+	done <<< "$WP_OPTIONS"
 }
 
 function import {
-  wp plugin is-installed wordpress-importer || install_a plugin <<< "wordpress-importer"
-  wp plugin activate wordpress-importer
-  # wp option update siteurl "$WP_URL"
-  # wp option update home "$WP_URL"
-  echo 'Importing, this may take a *very* long time.'
-  wp import $WP_IMPORT --authors=create --skip=image_resize --quiet "$@"
+	wp plugin is-installed wordpress-importer || install_a plugin <<< "wordpress-importer"
+	wp plugin activate wordpress-importer
+	# wp option update siteurl "$WP_URL"
+	# wp option update home "$WP_URL"
+	echo 'Importing, this may take a *very* long time.'
+	wp import $WP_IMPORT --authors=create --skip=image_resize --quiet "$@"
 }
 
-function usage {
-  echo <<-EOT
-    Usage: entrypoing.sh [-v -i -m] [command]
-    
-    Options:
-    -v\t\tVerbose logging.
-    -i\t\tInstall Wordpress.
-    -m\t\tImport from \$WP_IMPORT if specified.
-
-EOT
-}
-
-# Parse options
-while getopts vimo OPT; do
-  case $OPT in
-    v) VERBOSE=true;;
-    i) install;;
-    m) import;;
-    o) options;;
-    *) usage; exit 1;;
-  esac
-done
-
-# Ensure proper ownership of the docroot
-chown -cR www-data:www-data "${BASE_DIR:-$DEFAULT_BASE_DIR}"
+set -e
+#if [[ "$1" == "apache2*" ]]
+#then
+	#env | egrep '^(WP|BB|MYSQL)_' | sort
+	env | sort
+	# Ensure proper ownership of the workdir.
+	install_core
+	upgrade
+	install_themes
+	install_plugins
+	wp plugin activate --all
+	options
+	chown -R www-data:www-data . 
+#fi 
 
 # Execute default function or command.
-log "${@:$OPTIND}"
-
-exec "${@:$OPTIND}"
+exec "${@}"
